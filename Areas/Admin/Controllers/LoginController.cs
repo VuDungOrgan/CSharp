503e25429671d485286c8e9d88e317197084cd18
Vudung201 <1911505310109@sv.ute.udn.vn>
2022-04-23 09:21:02 +0700

    new update 23/4

--- Areas/Admin/Controllers/LoginController.cs
+++ Areas/Admin/Controllers/LoginController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DemoWeb.Areas.Admin.Models;
+using Models_SVOnline;
+using Models_SVOnline.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +15,28 @@ namespace DemoWeb.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(loginModal loginModal)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new UserDao();
+                var result = user.login(loginModal.username, loginModal.password);
+                if(result == 1)
+                {
+                    //ModelState.AddModelError("","Dang Nhap Thanh Cong");
+                    Session.Add(Contants.USER_SESSION,loginModal.username);
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Dang Nhap That Bai");
+                }
+            }
+            return View("Index");
         }
     }
 }
