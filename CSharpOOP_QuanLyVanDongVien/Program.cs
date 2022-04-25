a71bee6480a6e16154b190f8b850a613c1b20972
Vudung <nguyenvudung030121@gmail.com>
2022-04-25 20:57:59 +0700

    new

--- CSharpOOP_QuanLyVanDongVien/Program.cs
+++ CSharpOOP_QuanLyVanDongVien/Program.cs
@@ -6,6 +6,44 @@ using System.Threading.Tasks;
 
 namespace CSharpOOP_QuanLyVanDongVien
 {
+
+    class VanDongVien
+    {
+        private string hoten;
+        private string monthidau;
+        private int tuoi;
+        private double cannang;
+        private double chieucao;
+
+        public VanDongVien()
+        {
+            hoten = "";
+            monthidau = "";
+            tuoi = 0;
+            cannang = 0;
+            chieucao = 0;
+        }
+
+        public VanDongVien(string hoten, string monthidau, int tuoi, double cannang, double chieucao)
+        {
+            this.hoten = hoten;
+            this.monthidau = monthidau;
+            this.tuoi = tuoi;
+            this.cannang = cannang;
+            this.chieucao = chieucao;
+        }
+
+        ~VanDongVien()
+        {
+            hoten = "";
+            monthidau = "";
+            tuoi = 0;
+            cannang = 0;
+            chieucao = 0;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
