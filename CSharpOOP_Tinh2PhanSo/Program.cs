df66026aa608ee6869ec926597f2f39eace30dc7
Vudung <nguyenvudung030121@gmail.com>
2022-04-25 16:13:34 +0700

    new

--- CSharpOOP_Tinh2PhanSo/Program.cs
+++ CSharpOOP_Tinh2PhanSo/Program.cs
@@ -6,6 +6,39 @@ using System.Threading.Tasks;
 
 namespace CSharpOOP_Tinh2PhanSo
 {
+    class PhanSo
+    {
+        private int tuso;
+        private int mauso;
+
+        public PhanSo()
+        {
+            tuso = 0;
+            mauso = 1;
+        }
+
+        ~PhanSo()
+        {
+            tuso = 0;
+            mauso = 1;
+        }
+
+        public int Tuso {
+            get => tuso;
+            set => tuso = value;
+        }
+        public int Mauso {
+            get => mauso;
+            set => mauso = value;
+        }
+
+        public void nhap()
+        {
+            
+        }
+
+        
+    }
     class Program
     {
         static void Main(string[] args)
