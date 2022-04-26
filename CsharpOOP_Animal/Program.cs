9afa484b88b3a63f9f2957ede65b836b5e5d916e
Vudung <nguyenvudung030121@gmail.com>
2022-04-26 07:56:10 +0700

    new

--- CsharpOOP_Animal/Program.cs
+++ CsharpOOP_Animal/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace CsharpOOP_Animal
 {
+    interface IAnimal
+    {
+        void eat();
+        void makeSound();
+    }
+    class Cat : IAnimal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Con meeo dang an");
+        }
+
+        public void makeSound()
+        {
+            Console.WriteLine("Con meeo keu meo meo");   
+        }
+        public void run()
+        {
+            Console.WriteLine("Con meo dang chay...");
+        }
+    }
+    class Bird : IAnimal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Con chim dang an");
+        }
+
+        public void makeSound()
+        {
+            Console.WriteLine("Con chim keu cuc cu");
+        }
+        public void fly()
+        {
+            Console.WriteLine("Con chim dang bay...");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Bird conChim = new Bird();
+            conChim.fly();
+
+            Cat conMeo = new Cat();
+            conMeo.run();
+
+            Console.ReadKey();
         }
     }
 }
