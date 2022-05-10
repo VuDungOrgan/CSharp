750b462408f18febe39e41786a389db39da10b4b
Vudung <nguyenvudung030121@gmail.com>
2022-05-10 07:25:34 +0700

    Add project files.

---
+++ CSharp_Mang/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Mang
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Nhap vao A[{0}]: ",i+1);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
