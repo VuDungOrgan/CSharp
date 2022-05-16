98b76fbbd7c91a4142f07094d87b7fd2285ea677
Vudung <nguyenvudung030121@gmail.com>
2022-05-16 21:24:43 +0700

    Add project files.

---
+++ SortedListCSharp/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortedListCSharp
+{
+    public class Person
+    {
+        public Person()
+        {
+ 
+        }
+        
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        private string name;
+        private int age;
+        private string p;
+        private int p_2;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+          get { return name; }
+          set { name = value; }
+        }
+
+        public override string ToString()
+        {
+            return Name + " - " + Age;
+        }
+    }
+}
